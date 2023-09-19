bd70248ad10867b7991a6e9f2c356e88fcb39dc2
Matt Fergoda <mattfergoda@users.noreply.github.com>
2023-09-19 16:19:19 -0700

    WIP. GIFs not displaying correctly.

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log("Let's get this party started!");
+"use strict";
+
+async function getGif(evt) {
+  evt.preventDefault();
+
+  const searchWords = $("#search-words").val();
+  const endpoint = 'https://api.giphy.com/v1/gifs/search';
+  const apiKey = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym';
+
+  const params = new URLSearchParams({
+    api_key: apiKey,
+    q: searchWords,
+    limit: 20,
+  });
+
+  const response = await fetch(`${endpoint}?${params}`);
+  const data = await response.json();
+
+  console.log("getGif response=", response, "data=", data);
+  displayRandomGif(data.data)
+
+}
+
+function displayRandomGif(data) {
+  const randomGifUrl = data[0].url;
+  const gifImgElement = $('<img>').attr('src', randomGifUrl);
+  $('#gif-gallery').append(gifImgElement);
+}
+
+$('#submit-button').on("click", getGif);
