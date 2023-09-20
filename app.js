4ed2573165a9fa804cb304fdc75dd617209efa32
JaeWon Jin <j1jin286@gmail.com>
2023-09-19 18:10:42 -0700

    Complete

--- app.js
+++ app.js
@@ -1,45 +1,54 @@
 "use strict";
 
 const NUM_GIFS_REQUESTED = 20;
-// TODO: put endpoint and api key here. Split endpoint into base URL and everything else.
+const BASE_URL = 'https://api.giphy.com/v1';
+const API_KEY = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym';
+
 
 /**
- * Gets the user's search words and calls Giphy API with those search words.
- * Returns the response from the Giphy API as an object.
+ * Takes the user's search words and calls Giphy API with those search words.
+ * Returns an array of urls of GIFs.
  */
-async function searchGiphyAPI() {
-  const searchWords = $("#search-words").val();
-
+async function searchGiphyAPI(searchWords) {
   if (searchWords === '') throw new Error("Empty search string.");
 
-  const endpoint = 'https://api.giphy.com/v1/gifs/search';
-  const apiKey = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym';
+  const endpoint = "/gifs/search";
 
   const params = new URLSearchParams({
-    api_key: apiKey,
+    api_key: API_KEY,
     q: searchWords,
     limit: NUM_GIFS_REQUESTED,
   });
 
-  const response = await fetch(`${endpoint}?${params}`);
+  const response = await fetch(`${BASE_URL}${endpoint}?${params}`);
   const data = await response.json();
 
   console.log("getGif response=", response, "data=", data);
 
-  return data.data.map(); // TODO:
+  return data.data.map(function(gifObj){
+    return gifObj.images.original.url;
+  });
+}
+
+function getSearchWords(){
+  const searchWords = $("#search-words").val();
+  return searchWords;
 }
 
 /**
  * Takes a Giphy API response in the form of an object, picks
  * a random GIF from the object, and displays it in the DOM.
  */
-function displayRandomGif(data) {
-  const randomInt = Math.floor(Math.random() * NUM_GIFS_REQUESTED); // TODO: Could refactor this
+function displayRandomGif(urls) {
+  const randomInt = getRandomInt(NUM_GIFS_REQUESTED);
 
-  console.log("data: ", data[randomInt]);
-  const randomGifUrl = data[randomInt].images.original.url;
-  const gifImgElement = $('<img>').attr('src', randomGifUrl);
-  $('#gif-gallery').append(gifImgElement);
+  const $gifImgElement = $('<img>').attr('src', urls[randomInt]);
+  $('#gif-gallery').append($gifImgElement);
+}
+
+/**Takes an upper limit and gets a random integer between 0 and upper limit */
+function getRandomInt(upperLimit){
+  return Math.floor(Math.random() * upperLimit);
 }
 
 /**
@@ -48,8 +57,9 @@ function displayRandomGif(data) {
 async function handleSubmit(evt) {
   evt.preventDefault();
   try {
-    const data = await searchGiphyAPI();
-    displayRandomGif(data.data);
+    const searchWords = getSearchWords();
+    const urls = await searchGiphyAPI(searchWords);
+    displayRandomGif(urls);
   } catch(err) {
     console.warn(err);
     return;
