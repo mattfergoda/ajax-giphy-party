4baf323b26788c4353b7dae95beafbed4b615f6b
Matt Fergoda <mattfergoda@users.noreply.github.com>
2023-09-19 17:17:30 -0700

    All functionality for exercise works.

--- app.js
+++ app.js
@@ -1,33 +1,65 @@
 "use strict";
 
-async function getGif(evt) {
-  evt.preventDefault();
+const NUM_GIFS_REQUESTED = 20;
 
+/**
+ * Gets the user's search words and calls Giphy API with those search words.
+ * Returns the response from the Giphy API as an object.
+ */
+async function searchGiphyAPI() {
   const searchWords = $("#search-words").val();
+
+  //if (searchWords === '') throw new Error("Empty search string.");
+
   const endpoint = 'https://api.giphy.com/v1/gifs/search';
   const apiKey = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym';
 
   const params = new URLSearchParams({
     api_key: apiKey,
     q: searchWords,
-    limit: 20,
+    limit: NUM_GIFS_REQUESTED,
   });
 
   const response = await fetch(`${endpoint}?${params}`);
   const data = await response.json();
 
   console.log("getGif response=", response, "data=", data);
-  displayRandomGif(data.data)
 
+  return data;
 }
 
+/**
+ * Takes a Giphy API response in the form of an object, picks
+ * a random GIF from the object, and displays it in the DOM.
+ */
 function displayRandomGif(data) {
-  const randomInt = Math.floor(Math.random() * 20);
+  const randomInt = Math.floor(Math.random() * NUM_GIFS_REQUESTED);
 
-  console.log("data: ", data[randomInt])
+  console.log("data: ", data[randomInt]);
   const randomGifUrl = data[randomInt].images.original.url;
   const gifImgElement = $('<img>').attr('src', randomGifUrl);
   $('#gif-gallery').append(gifImgElement);
 }
 
-$('#submit-button').on("click", getGif);
+/**
+ * Handles the user submitting a Giphy search.
+ */
+async function handleSubmit(evt) {
+  evt.preventDefault();
+
+  const data = await searchGiphyAPI();
+  displayRandomGif(data.data);
+
+
+}
+
+/**
+ * Removes all GIFs from the gallery.
+ */
+function removeAllGifs(evt) {
+  evt.preventDefault();
+  $('#gif-gallery').empty();
+}
+
+$('#submit-button').on("click", handleSubmit);
+$('#remove-button').on("click", removeAllGifs);
